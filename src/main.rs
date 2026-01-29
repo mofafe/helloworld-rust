e1e0664cb9c49431f222c3be8650a298ef746b76
mofafe <hf3jd8@gmail.com>
2026-01-29 12:53:03 +0900

    FizzBuzzやるのにいらんところを削除

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 fn main() {
     for i in 1..101 {
-        let mut output: String = String::new();
         match (i % 3, i % 5) {
-            (0, 0) => output.push_str("FizzBuzz"),
-            (0, _) => output.push_str("Fizz"),
-            (_, 0) => output.push_str("Buzz"),
-            (_, _) => output.push_str(""),
-        }
-        if output.is_empty() {
-            println!("{}", i)
-        } else {
-            println!("{}", output)
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            (_, _) => println!("{}", i),
         }
     }
 }
