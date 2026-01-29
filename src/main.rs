2479b11b25a47e8b8619fdf7ee4dd85343e801da
mofafe <hf3jd8@gmail.com>
2026-01-29 12:43:08 +0900

    わかってきた

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     for i in 1..101 {
         let mut output: String = String::new();
-        if i % 3 == 0 {
-            output.push_str("Fizz");
-        }
-        if i % 5 == 0 {
-            output.push_str("Buzz");
+        match (i % 3, i % 5) {
+            (0, 0) => output.push_str("FizzBuzz"),
+            (0, _) => output.push_str("Fizz"),
+            (_, 0) => output.push_str("Buzz"),
+            (_, _) => output.push_str(""),
         }
         if output.is_empty() {
             println!("{}", i)
